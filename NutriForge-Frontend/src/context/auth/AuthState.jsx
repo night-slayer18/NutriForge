7ae791cc3f79ee134547afe0b6251d07bfae8925
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-01 22:25:56 +0530

    added error handling for auth context

--- NutriForge-Frontend/src/context/auth/AuthState.jsx
+++ NutriForge-Frontend/src/context/auth/AuthState.jsx
@@ -5,18 +5,30 @@ import { useState } from 'react';
 const AuthState = (props) => {
     const [userDetails, setUserDetails] = useState({name: localStorage.getItem('name'), email: localStorage.getItem('email'), id: localStorage.getItem('USER'), token: localStorage.getItem('token')})
     const fetchUser = async () => {
-        const response = await fetch('http://localhost:8000/api/auth/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'auth-token': localStorage.getItem('token')
-            }
-        })
-        const data = await response.json();
-        localStorage.setItem('name', data.user.name)
-        localStorage.setItem('email', data.user.email)
-        localStorage.setItem('USER', data.user._id)
-        setUserDetails({name: localStorage.getItem('name'), email: localStorage.getItem('email'), id: localStorage.getItem('USER'), token: localStorage.getItem('token')})
+        try {
+                const response = await fetch('http://localhost:8000/api/auth/user', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'auth-token': localStorage.getItem('token')
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                localStorage.setItem('name', data.user.name);
+                localStorage.setItem('email', data.user.email);
+                localStorage.setItem('USER', data.user._id);
+                setUserDetails({
+                    name: localStorage.getItem('name'),
+                    email: localStorage.getItem('email'),
+                    id: localStorage.getItem('USER'),
+                    token: localStorage.getItem('token')
+                });
+        } catch (error) {
+            console.error('Error fetching user:', error.message);
+        }
     }
     return (
         <AuthContext.Provider value={{userDetails,setUserDetails,fetchUser}}>
