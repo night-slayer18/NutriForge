3729db64594f58f4e28d480540510491f3f88d08
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-02 23:06:52 +0530

    added error handling for food context

--- NutriForge-Frontend/src/context/food/FoodState.jsx
+++ NutriForge-Frontend/src/context/food/FoodState.jsx
@@ -1,129 +1,156 @@
 import PropTypes from 'prop-types';
-import FoodContext from "./FoodContext"
+import FoodContext from "./FoodContext";
 import { useState } from 'react';
 
 const FoodState = (props) => {
     const host = "http://localhost:8000";
-    const foodInitial = []
-    const trackInitial = []
-    const trackFoodByDateInitial = []
-    const searchFoodInitial = []
-    const [foods,setFoods] = useState(foodInitial)
-    const [trackfood,settrackFood] = useState(trackInitial)
-    const [trackFoodByDate,settrackFoodByDate] = useState(trackFoodByDateInitial)
-    const [searchFood,setSearchFood] = useState(searchFoodInitial)
-
+    const foodInitial = [];
+    const trackInitial = [];
+    const trackFoodByDateInitial = [];
+    const searchFoodInitial = [];
+    const [foods, setFoods] = useState(foodInitial);
+    const [trackfood, settrackFood] = useState(trackInitial);
+    const [trackFoodByDate, settrackFoodByDate] = useState(trackFoodByDateInitial);
+    const [searchFood, setSearchFood] = useState(searchFoodInitial);
 
     const getFoods = async () => {
-        const reponse = await fetch(`${host}/api/food/getfood`,{
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            }
-        })
-        const json = await reponse.json()
-        setFoods(json.food)
-    }
+        try {
+            const response = await fetch(`${host}/api/food/getfood`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                }
+            });
+            const json = await response.json();
+            setFoods(json.food);
+        } catch (error) {
+            console.error("Failed to fetch foods:", error);
+        }
+    };
 
-    const addFood =  async (name,calories,protein,carbohydrates,fat,fiber) => {
-        const reponse = await fetch(`${host}/api/food/addfood`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify({name,calories,protein,carbohydrates,fat,fiber})
-        })
-        const json = await reponse.json()
-        setFoods(foods.concat(json.savedFood))
-    }
+    const addFood = async (name, calories, protein, carbohydrates, fat, fiber) => {
+        try {
+            const response = await fetch(`${host}/api/food/addfood`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                },
+                body: JSON.stringify({ name, calories, protein, carbohydrates, fat, fiber })
+            });
+            const json = await response.json();
+            setFoods(foods.concat(json.savedFood));
+        } catch (error) {
+            console.error("Failed to add food:", error);
+        }
+    };
 
     const searchFoodByName = async (name) => {
-        const response = await fetch (`${host}/api/food/searchfood/${name}`,{
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            }
-        })
-        const json = await response.json()
-        setSearchFood(json.food)
-    }
-
-    const updateFood = async (id,name,calories,protein,carbohydrates,fat,fiber) => {
-        const response = await fetch(`${host}/api/food/updatefood/${id}`,{
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify({name,calories,protein,carbohydrates,fat,fiber})
-        })
-        const json = await response.json()
-        console.log(json)
-        let newFoods = JSON.parse(JSON.stringify(foods))
-        for (let index = 0; index < newFoods.length; index++) {
-            const element = newFoods[index];
-            if(element._id === id){
-                newFoods[index].name = name
-                newFoods[index].calories = calories
-                newFoods[index].protein = protein
-                newFoods[index].carbohydrates = carbohydrates
-                newFoods[index].fat = fat
-                newFoods[index].fiber = fiber
-                break
-            }
+        try {
+            const response = await fetch(`${host}/api/food/searchfood/${name}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                }
+            });
+            const json = await response.json();
+            setSearchFood(json.food);
+        } catch (error) {
+            console.error("Failed to search food by name:", error);
         }
-        setFoods(newFoods)
-    }
+    };
+
+    const updateFood = async (id, name, calories, protein, carbohydrates, fat, fiber) => {
+        try {
+            const response = await fetch(`${host}/api/food/updatefood/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                },
+                body: JSON.stringify({ name, calories, protein, carbohydrates, fat, fiber })
+            });
+            const json = await response.json();
+            console.log(json.food);
+            let newFoods = JSON.parse(JSON.stringify(foods));
+            for (let index = 0; index < newFoods.length; index++) {
+                const element = newFoods[index];
+                if (element._id === id) {
+                    newFoods[index].name = name;
+                    newFoods[index].calories = calories;
+                    newFoods[index].protein = protein;
+                    newFoods[index].carbohydrates = carbohydrates;
+                    newFoods[index].fat = fat;
+                    newFoods[index].fiber = fiber;
+                    break;
+                }
+            }
+            setFoods(newFoods);
+        } catch (error) {
+            console.error("Failed to update food:", error);
+        }
+    };
 
     const deleteFood = async (id) => {
-        const response = await fetch(`${host}/api/food/deletefood/${id}`,{
-            method: "DELETE",
-        })
-        const json = await response.json()
-        console.log(json.food)
-        console.log("deleted a food item" + id)
-        const newFoods = foods.filter((food) => {return food._id !== id})
-        setFoods(newFoods)
-    }
+        try {
+            const response = await fetch(`${host}/api/food/deletefood/${id}`, {
+                method: "DELETE",
+            });
+            const json = await response.json();
+            console.log(json.food);
+            console.log("deleted a food item" + id);
+            const newFoods = foods.filter((food) => { return food._id !== id });
+            setFoods(newFoods);
+        } catch (error) {
+            console.error("Failed to delete food:", error);
+        }
+    };
 
-    const trackFood = async(foodID,quantity) => {
-        const response = await fetch(`${host}/api/food/trackfood`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify({foodID,quantity})
-        })
-        const json = await response.json()
-        console.log(json)
-        settrackFood(trackfood.concat(json.savedTrack))
-    }
+    const trackFood = async (foodID, quantity) => {
+        try {
+            const response = await fetch(`${host}/api/food/trackfood`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                },
+                body: JSON.stringify({ foodID, quantity })
+            });
+            const json = await response.json();
+            console.log(json);
+            settrackFood(trackfood.concat(json.savedTrack));
+        } catch (error) {
+            console.error("Failed to track food:", error);
+        }
+    };
 
-    const trackFoodDate = async(date) => {
-        const response = await fetch(`${host}/api/food/gettrack/${localStorage.getItem('USER')}/${date}`,{
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            }
-        })
-        const json = await response.json()
-        settrackFoodByDate(json.track)
-    }
+    const trackFoodDate = async (date) => {
+        try {
+            const response = await fetch(`${host}/api/food/gettrack/${localStorage.getItem('USER')}/${date}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "auth-token": localStorage.getItem("token")
+                }
+            });
+            const json = await response.json();
+            settrackFoodByDate(json.track);
+        } catch (error) {
+            console.error("Failed to track food by date:", error);
+        }
+    };
 
     return (
-        <FoodContext.Provider value={{foods,getFoods,addFood,searchFoodByName,updateFood,deleteFood,trackFood,trackFoodByDate,trackFoodDate,searchFood,setSearchFood}}>
-                {props.children}
+        <FoodContext.Provider value={{ foods, getFoods, addFood, searchFoodByName, updateFood, deleteFood, trackFood, trackFoodByDate, trackFoodDate, searchFood, setSearchFood }}>
+            {props.children}
         </FoodContext.Provider>
-    )
-}
+    );
+};
 
 FoodState.propTypes = {
     children: PropTypes.node.isRequired
 };
 
-export default FoodState
+export default FoodState;
